8070f05a1075c145027466400eeaeb71b98431a4
patrick lesaint <patrick.lesaint@atos.net>
2017-02-13 23:59:23 +0100

    change articles photos + optimisations

--- routes/backend.js
+++ routes/backend.js
@@ -4,21 +4,13 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
     res.redirect('index');
-});
-
-router.get('/index', function(req, res, next) {
+    }).get('/index', function(req, res, next) {
     res.render('backend/index');
-})
-
-router.get('/ajout-edit', function(req, res, next) {
+    }).get('/ajout-edit', function(req, res, next) {
     res.render('backend/ajout-edit');
-});
-
-router.get('/factures', function(req, res, next) {
+    }).get('/factures', function(req, res, next) {
     res.render('backend/factures');
-});
-
-router.get('', function(req, res, next) {
+    }).get('', function(req, res, next) {
     res.render('facture/facturePDF');
 });
 
